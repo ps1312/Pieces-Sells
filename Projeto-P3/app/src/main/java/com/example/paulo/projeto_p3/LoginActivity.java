6e396b2faf0687f26445df18aa26917db24e78e9
ps1312 <ps1312@gmail.com>
2018-05-21 18:19:01 -0300

    adding primitive auth activities, layouts and permissions"

---
+++ Projeto-P3/app/src/main/java/com/example/paulo/projeto_p3/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.example.paulo.projeto_p3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public String TAG = "ERROR_________";
+    private EditText loginInput;
+    private EditText passwordInput;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Parse.initialize(getApplicationContext());
+
+        loginInput = findViewById(R.id.username_input_login);
+        passwordInput = findViewById(R.id.password_input_login);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "LOGGIN USER..");
+                //TODO checar se inputs estao vazios
+                ParseUser.logInInBackground(String.valueOf(loginInput.getText()), String.valueOf(passwordInput.getText()), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Toast.makeText(getApplicationContext(), "Logged in with success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.i(TAG, "Error login user");
+                            ParseUser.logOut();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ Projeto-P3/app/src/main/java/com/example/paulo/projeto_p3/SignUpActivity.java
@@ -0,0 +1,69 @@
+package com.example.paulo.projeto_p3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+
+public class SignUpActivity extends AppCompatActivity {
+    public String TAG = "ERROR_________";
+    private EditText loginInput;
+    private EditText passwordInput;
+    private EditText confirmPassInput;
+    private Button registerButton;
+    private Button loginActivityButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Parse.initialize(this);
+
+        loginInput = findViewById(R.id.username_input_register);
+        passwordInput = findViewById(R.id.password_input_register);
+        confirmPassInput = findViewById(R.id.confirm_pass_input_register);
+        registerButton = findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO Checar se as credenciais estao vazias e se as senhas dao match
+                ParseUser user = new ParseUser();
+                user.setUsername(String.valueOf(loginInput.getText()));
+                user.setPassword(String.valueOf(passwordInput.getText()));
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null){
+                            Toast.makeText(getApplicationContext(), "User created with success, welcome.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            e.printStackTrace();
+                            ParseUser.logOut();
+                        }
+                    }
+                });
+            }
+        });
+
+        loginActivityButton = findViewById(R.id.login_activity_button);
+
+        loginActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(loginActivity);
+            }
+        });
+
+    }
+}
