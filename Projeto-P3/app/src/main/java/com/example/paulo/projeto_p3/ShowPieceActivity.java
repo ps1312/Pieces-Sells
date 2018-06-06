cd851cffcfc0bec965955eade0e72b7ba4863033
ps1312 <ps1312@gmail.com>
2018-06-06 19:11:05 -0300

    adding status button funcionality

--- Projeto-P3/app/src/main/java/com/example/paulo/projeto_p3/ShowPieceActivity.java
+++ Projeto-P3/app/src/main/java/com/example/paulo/projeto_p3/ShowPieceActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ShowPieceActivity extends AppCompatActivity {
@@ -11,6 +13,8 @@ public class ShowPieceActivity extends AppCompatActivity {
     private TextView itemNameTv;
     private TextView itemDescTv;
     private TextView itemQuantityTv;
+    private TextView pieceStatusTv;
+    private Button buyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +29,20 @@ public class ShowPieceActivity extends AppCompatActivity {
         itemNameTv = findViewById(R.id.piece_name_tv);
         itemDescTv = findViewById(R.id.piece_desc_tv);
         itemQuantityTv = findViewById(R.id.piece_quantity_tv);
+        pieceStatusTv = findViewById(R.id.piece_status_tv);
+        buyButton = findViewById(R.id.status_button);
 
         itemNameTv.setText(itemNome);
         itemDescTv.setText(itemDesc);
         itemQuantityTv.setText(itemQuantity);
+
+
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pieceStatusTv.setText("Comprado");
+                buyButton.setEnabled(false);
+            }
+        });
     }
 }
